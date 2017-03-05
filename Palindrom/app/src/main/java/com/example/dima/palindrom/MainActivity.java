dfa8e12a7db417ad81c70a46d94dc2fcbb1a3d2f
swodd <swodd@mail.ru>
2017-03-05 20:01:33 +0200

    Palindrom
    Create Project

---
+++ Palindrom/app/src/main/java/com/example/dima/palindrom/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.dima.palindrom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn;
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                String sb = new StringBuilder(s).reverse().toString();
+
+                if( s.equals(sb)){
+                    Toast.makeText(getApplicationContext(), "Yes, it's palindrom", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Not palindrom", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+
+
+}
