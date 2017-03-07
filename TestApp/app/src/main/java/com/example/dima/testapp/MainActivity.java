232719095725861a88f7afcad5ce5fcefd666a7b
swodd <swodd@mail.ru>
2017-03-07 20:08:10 +0200

    VkApp
    Add List Users

--- TestApp/app/src/main/java/com/example/dima/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/dima/testapp/MainActivity.java
@@ -1,20 +1,69 @@
 package com.example.dima.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
 import com.vk.sdk.util.VKUtil;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] scope = new String[]{VKScope.FRIENDS, VKScope.MESSAGES, VKScope.WALL};
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
-        System.out.println(Arrays.asList(fingerprints));
+
+        VKSdk.login(this, scope);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                listView = (ListView)findViewById(R.id.listView);
+                VKRequest request = VKApi.friends().get(VKParameters.from(VKApiConst.FIELDS, "first_name, last_name"));
+                request.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+
+                        VKList list = (VKList) response.parsedModel;
+
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_expandable_list_item_1,list);
+                        listView.setAdapter(arrayAdapter);
+                    }
+                });
+            }
+            @Override
+            public void onError(VKError error) {
+// Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
